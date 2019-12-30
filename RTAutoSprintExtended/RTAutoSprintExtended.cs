55ae3da649042039f58a232c987c01c67c6c8371
JohnEdwa <johnedwa@gmail.com>
2019-12-31 01:50:39 +0200

    Fixed, tweaked and extended RT AutoSprint.
    Original: https://thunderstore.io/package/Relocity3E8/RT_AutoSprint/

---
+++ RTAutoSprintExtended/RTAutoSprintExtended.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Reflection;
+using System.Collections;
+using System.Linq;
+using BepInEx;
+using BepInEx.Configuration;
+using Rewired;
+using UnityEngine;
+using R2API.Utils;
+using RoR2;
+
+
+
+
+namespace RT_AutoSprint
+{
+	[BepInDependency("com.bepis.r2api")]
+	[BepInPlugin(PluginGuid, pluginName, pluginVersion)]
+
+	public class RT_AutoSprintMod : BaseUnityPlugin
+	{
+		public const string PluginGuid = "com.RelocityThrawnarchMods.RT_AutoSprint_EXTENDED" + pluginName;
+        private const string pluginName = "RT_AutoSprint_EXTENDED";
+		private const string pluginVersion = "4478858.1.0";
+
+		private static ConfigWrapper<bool> ArtificerFlamethrowerToggle;
+
+		private static double RT_num;
+		public static bool RT_autoSprint;
+		public static bool RT_flameOn;
+
+		public void Awake()
+		{
+			RT_AutoSprintMod.RT_num = 0.0;
+
+		// Configuration
+			On.RoR2.Console.Awake += (orig, self) => {
+				CommandHelper.RegisterCommands(self);
+				orig(self);
+			};
+
+			ArtificerFlamethrowerToggle = Config.Wrap<bool>(
+				"Artificer",
+				"ArtificerFlamethrowerToggle",
+				"Sprinting cancels the flamethrower, therefore it either has to disable AutoSprint for a moment, or you need to keep the button held down\ntrue: Flamethrower is a toggle, cancellable by hitting Sprint or casting M2\nfalse: Flamethrower is cast when the button is held down (binding to side mouse button recommended).",
+				true
+			);
+
+		// Artificer Flamethrower logic
+			On.EntityStates.Mage.Weapon.Flamethrower.OnEnter += (orig, self) => {
+				if (ArtificerFlamethrowerToggle.Value) {
+					RT_AutoSprintMod.RT_flameOn = true;
+				} else RT_AutoSprintMod.RT_flameOn = false;
+				orig(self);
+			};
+			On.EntityStates.Mage.Weapon.Flamethrower.FixedUpdate += (orig, self) => {
+				if (Input.GetKeyDown(KeyCode.LeftShift)) {
+					RT_AutoSprintMod.RT_flameOn = false;
+				}
+				orig(self);
+			};
+
+			On.EntityStates.Mage.Weapon.Flamethrower.OnExit += (orig, self) => {
+				RT_AutoSprintMod.RT_flameOn = false;
+				orig(self);
+			};
+
+		// Sprinting logic
+			On.RoR2.PlayerCharacterMasterController.FixedUpdate += delegate(On.RoR2.PlayerCharacterMasterController.orig_FixedUpdate orig, RoR2.PlayerCharacterMasterController self) {
+				RT_AutoSprintMod.RT_autoSprint = false;
+				bool flag = false;
+				RoR2.NetworkUser networkUser = self.networkUser;
+				RoR2.InputBankTest instanceField = self.GetInstanceField<RoR2.InputBankTest>("bodyInputs");
+				bool flag2 = instanceField;
+				if (flag2) {
+					bool flag3 = networkUser && networkUser.localUser != null && !networkUser.localUser.isUIFocused;
+					if (flag3) {
+						Player inputPlayer = networkUser.localUser.inputPlayer;
+						RoR2.CharacterBody instanceField2 = self.GetInstanceField<RoR2.CharacterBody>("body");
+						bool flag4 = instanceField2;
+						if (flag4) {
+							RT_AutoSprintMod.RT_autoSprint = instanceField2.isSprinting;
+							bool flag5 = !RT_AutoSprintMod.RT_autoSprint;
+							if (flag5) {
+								bool flag6 = RT_AutoSprintMod.RT_num > 0.1;
+								bool flag7 = flag6;
+								if (flag7) {
+									RT_AutoSprintMod.RT_autoSprint = !RT_AutoSprintMod.RT_autoSprint;
+									RT_AutoSprintMod.RT_num = 0.0;
+								}
+								bool flag8 = instanceField2.baseNameToken == "MAGE_BODY_NAME";
+								if (flag8) {
+									flag = (!inputPlayer.GetButton("PrimarySkill") && !inputPlayer.GetButton("SpecialSkill") && !inputPlayer.GetButton("UtilitySkill") && !RT_AutoSprintMod.RT_flameOn);
+								} else {
+									bool flag9 = instanceField2.baseNameToken == "ENGI_BODY_NAME";
+									if (flag9) {
+										flag = (!inputPlayer.GetButton("SecondarySkill") && !inputPlayer.GetButton("SpecialSkill") && !inputPlayer.GetButton("UtilitySkill"));
+									} else {
+										bool flag10 = instanceField2.baseNameToken == "HUNTRESS_BODY_NAME";
+										if (flag10) {
+											flag = (!inputPlayer.GetButton("SecondarySkill") && !inputPlayer.GetButton("SpecialSkill"));
+										} else {
+											bool flag11 = instanceField2.baseNameToken == "MERC_BODY_NAME";
+											if (flag11) {
+												flag = (!inputPlayer.GetButton("SecondarySkill") && !inputPlayer.GetButton("SpecialSkill"));
+											} else {
+												flag = (!inputPlayer.GetButton("PrimarySkill") && !inputPlayer.GetButton("SecondarySkill") && !inputPlayer.GetButton("SpecialSkill"));
+											}
+										}
+									}
+								}
+							}
+							bool flag12 = flag;
+							if (flag12) {
+								RT_AutoSprintMod.RT_num += (double)Time.deltaTime;
+							} else {
+								RT_AutoSprintMod.RT_num = 0.0;
+							}
+							bool rt_autoSprint = RT_AutoSprintMod.RT_autoSprint;
+							if (rt_autoSprint) {
+								Vector3 aimDirection = instanceField.aimDirection;
+								aimDirection.y = 0f;
+								aimDirection.Normalize();
+								Vector3 moveVector = instanceField.moveVector;
+								moveVector.y = 0f;
+								moveVector.Normalize();
+								bool flag13 = Vector3.Dot(aimDirection, moveVector) < self.GetInstanceField<float>("sprintMinAimMoveDot");
+								if (flag13) {
+									RT_AutoSprintMod.RT_autoSprint = false;
+								}
+							}
+						}
+					}
+				}
+				orig.Invoke(self);
+				bool flag14 = instanceField;
+				if (flag14) {
+					instanceField.sprint.PushState(RT_AutoSprintMod.RT_autoSprint);
+				}
+			};
+			Debug.Log("Loaded RT AutoSprint Extended\nArtificer flamethrower mode is " + ((ArtificerFlamethrowerToggle.Value) ? " [toggle]." : " [hold]."));
+			//RoR2.Chat.AddMessage("Loaded RT AutoSprint Extended\nArtificer flamethrower mode is " + ((ArtificerFlamethrowerToggle.Value) ? " [toggle]." : " [hold]."));
+		}
+
+		/*
+		public void Update() {
+            if (Input.GetKeyDown(KeyCode.F2)) {
+				ArtificerFlamethrowerToggle.Value = !ArtificerFlamethrowerToggle.Value;
+				RoR2.Chat.AddMessage("Flamethrower Toggle: " + ArtificerFlamethrowerToggle.Value);
+			}
+        }
+		*/
+
+
+		[RoR2.ConCommand(commandName = "rt_artiflamemode", flags = ConVarFlags.None, helpText = "Artificer Flamethrower Mode: Toggle or Hold")]
+        private static void RTArtiFlameMode(RoR2.ConCommandArgs args) {
+			args.CheckArgumentCount(1);
+			switch (args[0].ToLower()) {
+				case "toggle":
+				case "true":
+				case "1":
+					ArtificerFlamethrowerToggle.Value = true;
+					break;
+				case "hold":
+				case "false":
+				case "0":
+					ArtificerFlamethrowerToggle.Value = false;
+					break;
+				default:
+					Debug.Log("Invalid argument. Valid argument: true/false, toggle/hold, 1/0");
+					break;
+			}
+			Debug.Log($"Artificer flamethrower mode is " + ((ArtificerFlamethrowerToggle.Value) ? " [toggle]." : " [hold]."));
+		}
+	}
+
+	// Helper classes
+
+	public class CommandHelper {
+        public static void RegisterCommands(RoR2.Console self) {
+            var types = typeof(CommandHelper).Assembly.GetTypes();
+            var catalog = self.GetFieldValue<IDictionary>("concommandCatalog");
+            foreach (var methodInfo in types.SelectMany(x => x.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic))) {
+                var customAttributes = methodInfo.GetCustomAttributes(false);
+                foreach (var attribute in customAttributes.OfType<RoR2.ConCommandAttribute>()) {
+                    var conCommand = Reflection.GetNestedType<RoR2.Console>("ConCommand").Instantiate();
+                    conCommand.SetFieldValue("flags", attribute.flags);
+                    conCommand.SetFieldValue("helpText", attribute.helpText);
+                    conCommand.SetFieldValue("action", (RoR2.Console.ConCommandDelegate)Delegate.CreateDelegate(typeof(RoR2.Console.ConCommandDelegate), methodInfo));
+                    catalog[attribute.commandName.ToLower()] = conCommand;
+                }
+            }
+        }
+    }
+
+    public static class Utils
+	{
+		public static T GetInstanceField<T>(this object instance, string fieldName) {
+			BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+			FieldInfo field = instance.GetType().GetField(fieldName, bindingAttr);
+			return (T)((object)field.GetValue(instance));
+		}
+		public static void SetInstanceField<T>(this object instance, string fieldName, T value) {
+			BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+			FieldInfo field = instance.GetType().GetField(fieldName, bindingAttr);
+			field.SetValue(instance, value);
+		}
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,70 @@
+# Auto Sprint Extended
+#### Fixed by JohnEdwa, Original by Relocity and Thrawnarch
+
+A fork/reverse engineered/extended RT AutoSprint. The original hasn't been updated in 8 months and is very broken, and there is no source or contact info.   
+This is a fixed version for the Hidden Realms update, build ID 4478858.
+
+
+## Differences to the old version
+* Works, for one thing
+* Artificer Flamethrower mode is configurable.
+
+## Configuration
+* Config file item: `ArtificerFlamethrowerToggle: true/false`: Sets the flamethrower mode. Default is toggle cancellable by pressing the sprint key, alternative is a "Hold to cast" mode.
+* Console command: `rt_artiflamemode`. toggle/true/1, or hold/false/0.
+
+## Known (new) Issues
+
+* Acrid M1 animation cancellation is very pronounced with this mod. Waiting for the official fix before trying for a workaround.
+
+
+## Changelog
+
+'4478858.1.0' - Initial Version (Based on 1.0.5), fixed, and added Artificer flamethrower config.
+
+---
+
+[//]: # (Thanks to FunkFrog and Sipondo for letting us use their README as a basis for this one. You're doing god's work.)
+
+# Auto Sprint
+#### By Relocity and Thrawnarch
+
+This mod has been developed to allow easy sprinting, without affecting game balance.
+
+## Features
+
+Whenever you could be sprinting in the base game, this mod makes you sprint. Includes special cases:
+
+- You stop sprinting briefly when attacking.
+
+- Huntress and Merc sprint while using basic (LMB) skills.
+
+- You sprint while charging Artificer M2 or Engineer M1, after a very brief pause.
+
+- Does not cancel channeled skills like the Artificer ult.
+
+## Installation Guide
+
+- Copy the 'RT_AutoSprint.dll' file to your BepInEx plugins folder.
+
+- Never press sprint again.
+
+- (Optional) unbind your sprint button.
+
+## Known Issues
+
+- At the moment, any custom classes will not allow sprinting during skill use, except for the Utility skill (ie. the commando is the default sprinting behavior).
+
+### Changelog
+
+'1.0.5' - Hopefully fixed an issue that was throwing a ton of NullReferenceException reports, creating a ton of lag in multiplayer games.
+
+`1.0.4` - No longer cancels Artificer ult skill!
+
+`1.0.3` - Added a callout for a known issue with the Artificer's ult skill.
+
+`1.0.2` - Clarified readme, fixed formatting in changelog, we hope.
+
+`1.0.1` - Cleaned up code slightly.
+
+`1.0.0` - Initial release.

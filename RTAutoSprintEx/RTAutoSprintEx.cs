f58e51e7a2641448971d0b6eb7a1e5fd6171e411
JohnEdwa <johnedwa@gmail.com>
2021-04-06 23:30:57 +0300

    pop

--- RTAutoSprintEx/RTAutoSprintEx.cs
+++ RTAutoSprintEx/RTAutoSprintEx.cs
@@ -5,7 +5,7 @@ using BepInEx;
 using BepInEx.Configuration;
 using Rewired;
 using UnityEngine;
-using EnigmaticThunder.Util;
+using R2API.Utils;
 using RoR2;
 using R2API;
 using Mono.Cecil.Cil;
@@ -14,7 +14,7 @@ using EntityStates;
 
 namespace RTAutoSprintEx {
 //[NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
-[BepInDependency(EnigmaticThunder.EnigmaticThunder.guid, BepInDependency.DependencyFlags.HardDependency)]
+//[BepInDependency(EnigmaticThunder.EnigmaticThunder.guid, BepInDependency.DependencyFlags.HardDependency)]
 [BepInPlugin(GUID, NAME, VERSION)]
 
 public class RTAutoSprintEx : BaseUnityPlugin {
@@ -23,13 +23,8 @@ public class RTAutoSprintEx : BaseUnityPlugin {
 		GUID = "com.johnedwa." + NAME,
 		VERSION = "2.0.0";
 
-	private static double RT_num;
 	private static bool RT_enabled;
 	private static bool RT_isSprinting;
-	private static bool RT_cancelWithSprint;
-	private static bool RT_tempDisable;
-	private string[] RT_CustomSurvivors;
-	private bool RT_CustomSurvivorDisable;
 
 	internal HashSet<Type> statesWhichDisableSprint = new HashSet<Type>();
 
@@ -39,36 +34,32 @@ public class RTAutoSprintEx : BaseUnityPlugin {
 	}
 
 	public bool ShouldSprintBeDisabledOnThisBody(CharacterBody targetBody) {
-		//var currentState = targetBody.GetComponent<EntityStateMachine>()?.state;
-		var currentState = targetBody.GetComponent<EntityStateMachine>().state; // this is always EntityStates.GenericCharacterMain
-		Debug.LogWarning(currentState); 
+		var currentState = targetBody.GetComponent<EntityStateMachine>().state; 
+			// EntityStates.GenericCharacterMain
+			// EntityStates.Toolbot.ToolbotDualWield
+			// EntityStates.Toolbot.ToolbotDash
+		Debug.LogWarning("Character " + targetBody.name + " state is " + currentState); 
 		if(currentState == null) return false;
 		return statesWhichDisableSprint.Contains(currentState.GetType());
 	}
 
-
 	public void Awake() {
 
-		RTAutoSprintEx.RT_num = 0.0;
 		RTAutoSprintEx.RT_enabled = true;
 		RTAutoSprintEx.RT_isSprinting = false;
-		RTAutoSprintEx.RT_cancelWithSprint = false;
-		RTAutoSprintEx.RT_tempDisable = false;
-		RT_CustomSurvivorDisable = false;
 
-		RegisterSprintDisabler<EntityStates.Toolbot.FireNailgun>();
-		RegisterSprintDisabler<EntityStates.Toolbot.FireGrenadeLauncher>();
-		RegisterSprintDisabler<EntityStates.Toolbot.AimGrenade>();
-		RegisterSprintDisabler<EntityStates.Captain.Weapon.SetupAirstrike>();
-		RegisterSprintDisabler<EntityStates.Captain.Weapon.SetupSupplyDrop>();
+		R2API.Utils.CommandHelper.AddToConsoleWhenReady();
+
+		RegisterSprintDisabler<EntityStates.Toolbot.ToolbotDualWield>();
+
+		On.RoR2.CharacterBody.OnSkillActivated += (orig, self, GenericSkill) => { 
+			orig(self, GenericSkill); 
+			Debug.Log( GenericSkill.skillDef.skillName + " | Index: ");
+		};
 
 		On.RoR2.PlayerCharacterMasterController.FixedUpdate += delegate(On.RoR2.PlayerCharacterMasterController.orig_FixedUpdate orig, RoR2.PlayerCharacterMasterController self) {
 			orig.Invoke(self);
-
-			//RTAutoSprintEx.RT_isSprinting = false;
-			bool skillsAllowAutoSprint = false;
-			bool knownSurvivor = true;
-			
+			RTAutoSprintEx.RT_isSprinting = false;
 			RoR2.InputBankTest instanceFieldBodyInputs = self.GetInstanceField<RoR2.InputBankTest>("bodyInputs");
 			if (instanceFieldBodyInputs) {
 				RoR2.NetworkUser networkUser = self.networkUser;
@@ -76,41 +67,28 @@ public class RTAutoSprintEx : BaseUnityPlugin {
 					RoR2.CharacterBody instanceFieldBody = self.GetInstanceField<RoR2.CharacterBody>("body");
 					if (instanceFieldBody && RTAutoSprintEx.RT_enabled) {
 						Player inputPlayer = networkUser.localUser.inputPlayer;
-						RTAutoSprintEx.RT_isSprinting = instanceFieldBody.isSprinting;
-						switch(instanceFieldBody.baseNameToken){
-								case "COMMANDO_BODY_NAME":
-								case "HUNTRESS_BODY_NAME":
-								case "MERC_BODY_NAME":
-								case "LOADER_BODY_NAME":
-								case "CROCO_BODY_NAME":
-									skillsAllowAutoSprint = true;
-									break;
-								case "ENGI_BODY_NAME":	
-								case "TREEBOT_BODY_NAME":
-								case "TOOLBOT_BODY_NAME":
-								case "CAPTAIN_BODY_NAME":
-									skillsAllowAutoSprint = (!RTAutoSprintEx.RT_tempDisable);
-									break;								
-								case "MAGE_BODY_NAME":
-									skillsAllowAutoSprint = (!RTAutoSprintEx.RT_tempDisable);
-									break;
-								default:									
-									break;
-							}
-
-						
-						Debug.LogWarning(self.GetInstanceField<RoR2.CharacterBody>("body").GetComponent<EntityStateMachine>().state);
-
-						//ShouldSprintBeDisabledOnThisBody(instanceFieldBody);
+						if (!instanceFieldBody.isSprinting) {
+							if (!ShouldSprintBeDisabledOnThisBody(instanceFieldBody)) instanceFieldBodyInputs.sprint.PushState(true);
+						}
 					}
-				}
-
-				if (instanceFieldBodyInputs && RTAutoSprintEx.RT_enabled && knownSurvivor) {
-					instanceFieldBodyInputs.sprint.PushState(RTAutoSprintEx.RT_isSprinting);
 				}
 			}
 }; // End of FixedUpdate
 	} // End of Awake
+
+	[RoR2.ConCommand(commandName = "rt_enabled", flags = ConVarFlags.ExecuteOnServer, helpText = "args[0]=(bool)enabled")]
+	private static void cc_rt_enabled(ConCommandArgs args) {
+		try {
+			args.CheckArgumentCount(1);
+			if (TryParseBool(args[0], out bool result)) {
+				RTAutoSprintEx.RT_enabled = (bool)result;
+				Debug.Log($"{nameof(RTAutoSprintEx.RT_enabled)}={RTAutoSprintEx.RT_enabled}");
+			}	
+		} catch (Exception ex) { Debug.LogError(ex); }
+	}
+
+internal static bool TryParseBool(string input, out bool result){ if(bool.TryParse(input,out result)){return true;} if(int.TryParse(input,out int val)) { result = val > 0 ? true : false; return true; } return false;}
+
 } // End of class RTAutoSprintEx
 
 // Utilities
